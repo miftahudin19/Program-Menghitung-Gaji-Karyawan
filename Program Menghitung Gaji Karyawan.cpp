3a159143889cb956dfcde2932e73f95c87010c12
miftahudin19 <40747706+miftahudin19@users.noreply.github.com>
2018-12-17 21:37:03 +0700

    Add files via upload

---
+++ Program Menghitung Gaji Karyawan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int gaji,j;
+	cout <<"Masukan Berapa Jam Anda Kerja : ";
+	cin >>j;
+	
+	//Rumus
+	gaji = 5000 * j;
+	cout<<"Gaji yang Anda Dapatan Adalah = " <<gaji;
+	
+	return 0;
+}
+
+
+
+
